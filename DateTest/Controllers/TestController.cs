e2fe47c9cdbc1c30fef68738b77cd1329905b885
GET Academy <terje@kolderup.net>
2020-06-23 13:16:18 +0200

    Initial commit

---
+++ DateTest/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DateTest.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DateTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public Task<MyViewModel> Test()
+        {
+            return Task.FromResult(new MyViewModel {Date = DateTime.Now});
+        }
+    }
+}
---
+++ DateTest/Model/MyViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateTest.Model
+{
+    public class MyViewModel
+    {
+        public DateTime Date { get; set; }
+    }
+}
